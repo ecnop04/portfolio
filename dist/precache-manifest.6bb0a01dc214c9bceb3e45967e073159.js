10b849d63e6dce4afbbde99be0519ac2a4e2f425
ecnop04 <ecnop04@yahoo.com>
2019-10-30 10:11:26 +0800

    Add dist folder

---
+++ dist/precache-manifest.6bb0a01dc214c9bceb3e45967e073159.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "142e9c1053ade618eb3d",
+    "url": "/css/app.2f0ec117.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "191626d4b6438e3d5424610afa337a8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e92205d2f82c5cd6117",
+    "url": "/js/about.d8efd1c1.js"
+  },
+  {
+    "revision": "142e9c1053ade618eb3d",
+    "url": "/js/app.9fc3de08.js"
+  },
+  {
+    "revision": "94485995465c115d108d",
+    "url": "/js/chunk-vendors.c874c479.js"
+  },
+  {
+    "revision": "017b5d3dd8ae66da3519f8cae5b06931",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
